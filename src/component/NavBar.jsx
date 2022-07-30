74dd1396f814a33f1efd2ed2818748fd3630d0d8
aliefwijaya <alieffitriyanto92@gmail.com>
2022-07-31 00:47:10 +0700

    initial-file

---
+++ src/component/NavBar.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+
+const NavBar = () =>{
+    return(
+    <>
+    
+    </>
+    );
+}
+
+export default NavBar;
---
+++ src/component/SignUp.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+
+const SignUp = () =>{
+    return(
+    <>
+    
+    </>
+    );
+}
+
+export default SignUp;
